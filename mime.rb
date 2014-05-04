cdc5c1aac826780db0d6cac7548462cc3aeecd22
Drew Prentice <weirdpercent@gmail.com>
2014-05-04 13:08:50 -0400

    rubocop code cleanup

--- mime.rb
+++ mime.rb
@@ -1,25 +1,25 @@
-#xml='/usr/share/mime/packages/kde.xml'
-xml='kde.xml'
-mt=File.readlines(xml)
-start=mt.index("  <!-- all/ fake mime types -->\n")
+# xml = '/usr/share/mime/packages/kde.xml'
+xml = 'kde.xml'
+mt = File.readlines(xml)
+start = mt.index("  <!-- all/ fake mime types -->\n")
 start += 1
-finish=mt.index("    <comment>rtspu: URIs</comment>\n")
+finish = mt.index("    <comment>rtspu: URIs</comment>\n")
 finish += 1
-x=start
+x = start
 while x <= finish do
-  mt[x]=''
+  mt[x] = ''
   x += 1
 end
-nf=File.new("newkde.xml", "w")
-y=0
+nf = File.new("newkde.xml", "w")
+y = 0
 mt.each do
   nf.print mt[y]
   y += 1
 end
 nf.close
-puts "Run: diff ./kde.xml /usr/share/mime/packages/kde.xml"
-puts "Run: sudo mv newkde.xml /usr/share/mime/packages/kde.xml"
-puts "Run: update-mime-datebase /usr/share/mime"
+puts 'Run: diff ./kde.xml /usr/share/mime/packages/kde.xml'
+puts 'Run: sudo mv newkde.xml /usr/share/mime/packages/kde.xml'
+puts 'Run: update-mime-datebase /usr/share/mime'
 #`diff ./kde.xml ./newkde.xml`
 #`mv newkde.xml /usr/share/mime/packages/kde.xml`
 #`update-mime-database /usr/share/mime`
