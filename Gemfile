106b93ca28ae3f6c245b55c50dc92754c969abd7
Drew Prentice <weirdpercent@gmail.com>
2014-11-01 11:10:21 -0400

    use regex

--- Gemfile
+++ Gemfile
@@ -2,4 +2,4 @@ source 'http://rubygems.org'
 
 gem 'bundler'
 gem 'rake'
-
+gem 'pry'
--- mime.rb
+++ mime.rb
@@ -1,25 +1,19 @@
-#xml = '/usr/share/mime/packages/kde.xml'
-xml = 'kde.xml'
+xml = '/usr/share/mime/packages/kde.xml'
 mt = File.readlines(xml)
-start = mt.index("  <!-- all/ fake mime types -->\n")
-start += 1
-finish = mt.index("    <comment>rtspu: URIs</comment>\n")
-finish += 1
-x = start
-while x <= finish do
-  mt[x] = ''
-  x += 1
-end
+e = mt.scan(/(.*)(<!-- all\/ fake mime types -->)([<>A-Za-z\s"\/!:=-]*)(<!-- uri\/ fake mime types -->)([<>A-Za-z\s"\/!:=-]*<mime-type type="uri\/rtspu">\s*<comment>rtspu: URIs<\/comment>\s*<\/mime-type>)(.*)/m)
+e = e[0]
+ed = "#{e[0]}#{e[1]}\n\n  #{e[3]}#{e[5]}"
 nf = File.new("newkde.xml", "w")
-y = 0
-mt.each do
-  nf.print mt[y]
-  y += 1
-end
+nf.print ed
 nf.close
-puts 'Run: diff ./newkde.xml /usr/share/mime/packages/kde.xml'
-puts 'Run: sudo mv newkde.xml /usr/share/mime/packages/kde.xml'
-puts 'Run: sudo update-mime-datebase /usr/share/mime'
-#`diff ./kde.xml ./newkde.xml`
-#`mv newkde.xml /usr/share/mime/packages/kde.xml`
-#`update-mime-database /usr/share/mime`
+puts 'Lines to be removed:'
+`diff ./newkde.xml #{xml}`
+puts 'Do you want to save these changes?(y/n)'
+save = gets
+if save == 'y\n'
+  `sudo mv newkde.xml #{xml}`
+  `sudo update-mime-database /usr/share/mime`
+else
+  puts 'Cancelling changes.'
+  File.delete('newkde.xml')
+end
